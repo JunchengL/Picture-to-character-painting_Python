c6840734f656377139288efd86f93f520263204e
Juncheng LIU <juncheng.liu@outlook.com>
2019-04-23 23:13:25 +0200

    :sparkles: Change description

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Picture-to-character-painting_Python
 Python Projet for Picture to character painting
+
+
+
+python3 ascii.py picture_name
+
+
+
+Output will print on screen and save to output.txt file
--- ascii.py
+++ ascii.py
@@ -2,71 +2,72 @@
 from PIL import Image
 import argparse
 
-# 首先，构建命令行输入参数处理 ArgumentParser 实例
+# First, build a command line input handler the ArgumentParser instance.
 parser = argparse.ArgumentParser()
 
-# 定义输入文件、输出文件、输出字符画的宽和高
-parser.add_argument('file')     #输入文件
-parser.add_argument('-o', '--output')   #输出文件
-parser.add_argument('--width', type = int, default = 80) #输出字符画宽
-parser.add_argument('--height', type = int, default = 80) #输出字符画高
+# Define the width and height of the input file, output file, and output character
+parser.add_argument('file')     #Input File
+parser.add_argument('-o', '--output')   #Output File
+parser.add_argument('--width', type = int, default = 80) #Output character width
+parser.add_argument('--height', type = int, default = 80) #Output character height
 
-# 解析并获取参数
+# Parse and get parameters
 args = parser.parse_args()
 
-# 输入的图片文件路径
+# Input image file path
 IMG = args.file
 
-# 输出字符画的宽度
+# Output character width
 WIDTH = args.width
 
-# 输出字符画的高度
+# Output character height
 HEIGHT = args.height
 
-# 输出字符画的路径
+# Output path
 OUTPUT = args.output
 
 ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 
 def get_char(r,g,b,alpha = 256):
 
-    # 判断 alpha 值
+    # Judge alpha value
     if alpha == 0:
         return ' '
 
-    # 获取字符集的长度，这里为 70
+    # Get the length of the character set, here is 70
     length = len(ascii_char)
 
-    # 将 RGB 值转为灰度值 gray，灰度值范围为 0-255
+    # Convert RGB values to grayscale gray, with grayscale values ranging from 0-255
     gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
 
-    # 灰度值范围为 0-255，而字符集只有 70
-    # 需要进行如下处理才能将灰度值映射到指定的字符上
+    # Gray value range is 0-255, while character set is only 70
+    # Need to do the following to map the gray value to the specified character
     unit = (256.0 + 1)/length
 
-    # 返回灰度值对应的字符
+    # Returns the character corresponding to the gray value
     return ascii_char[int(gray/unit)]
+
 if __name__ == '__main__':
 
-    # 打开并调整图片的宽和高
+    # Open and adjust the width and height of the image
     im = Image.open(IMG)
     im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
 
-    # 初始化输出的字符串
+    # Initialize the output string
     txt = ""
 
-    # 遍历图片中的每一行
+    # Traverse each line in the picture
     for i in range(HEIGHT):
-        # 遍历该行中的每一列
+        # Traversing each column in the row
         for j in range(WIDTH):
-            # 将 (j,i) 坐标的 RGB 像素转为字符后添加到 txt 字符串
+            # Convert RGB pixels of (j,i) coordinates to characters and add them to txt string
             txt += get_char(*im.getpixel((j,i)))
-        # 遍历完一行后需要增加换行符
+        # Need to add a newline after traversing a line
         txt += '\n'
-    # 输出到屏幕
+    # Print to screen
     print(txt)
 
-    # 字符画输出到文件
+    # Output to file
     if OUTPUT:
         with open(OUTPUT,'w') as f:
             f.write(txt)
